897a630ef7f7f3d7501cd7ecf37972020d5dff6d
Manish Anand <manishanandprogrammer@gmail.com>
2021-05-24 06:58:43 +0530

    String Sub Sequence

---
+++ src/main/java/pepcoding/StringSubSequence.java
@@ -0,0 +1,29 @@
+package pepcoding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSubSequence {
+    public static void main(String[] args) {
+        System.out.println(subSequence("abc"));
+    }
+
+    private static List<String> subSequence(String value) {
+        if(value.length() == 0) {
+            List<String> baseCase = List.of("");
+            return baseCase;
+        }
+
+        char charAtZero = value.charAt(0);
+        String restOfTheString = value.substring(1);
+        List<String> resultOfRecursion = subSequence(restOfTheString);
+
+        List<String> finalList = new ArrayList<>();
+        for(String subSequence: resultOfRecursion) {
+            finalList.add(subSequence);
+            finalList.add(charAtZero + subSequence);
+        }
+
+        return finalList;
+    }
+}
